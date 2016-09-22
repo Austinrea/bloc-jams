80d883541e77ec7acc29903155bd1821a3c0d0fc
Austin James Rea <austinjrea@gmail.com>
2016-09-22 14:26:18 +0100

    Checkpoint 13 update missing code on player

--- scripts/album.js
+++ scripts/album.js
@@ -100,6 +100,11 @@ var clickHandler = function(targetElement) {
      } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
          songItem.innerHTML = playButtonTemplate;
          currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
      }
     
 };
