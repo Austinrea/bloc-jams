4c556238a4a32da8f86b5de8aa981f3d1b333e94
Austin James Rea <austinjrea@gmail.com>
2016-09-30 10:28:16 +0100

    checkpoint-21-assignment-final-submission

--- scripts/album.js
+++ scripts/album.js
@@ -119,17 +119,20 @@ var setCurrentAlbum = function(album) {
  var setCurrentTimeInPlayerBar = function(currentTime) {
     var $currentTimeElement = $('.current-time');
     $currentTimeElement.text(filterTimeCode(currentTime));
-};
+ };
 
-var setTotalTimeInPlayerBar = function(totalTime) {
+ var setTotalTimeInPlayerBar = function(totalTime) {
     var $totalTimeElement = $('.total-time');
     $totalTimeElement.text(filterTimeCode(totalTime));
-};
+ };
 
 var filterTimeCode = function(timeInSeconds) {
     var totalSeconds = parseFloat(timeInSeconds);
     var wholeMinutes = Math.floor(totalSeconds / 60);
     var wholeSeconds = Math.floor(totalSeconds - (wholeMinutes * 60));
+        if (wholeSeconds < 10) {
+            wholeSeconds = '0' + wholeSeconds;
+        };
     var timeFormatted = wholeMinutes + ':' + wholeSeconds;
         return timeFormatted;
 };
@@ -137,10 +140,11 @@ var filterTimeCode = function(timeInSeconds) {
  var updateSeekBarWhileSongPlays = function() {
      if (currentSoundFile) {
          currentSoundFile.bind('timeupdate', function(event) {
-             var seekBarFillRatio = this.getTime() / this.getDuration();
+             var currentTime = this.getTime();
+             var seekBarFillRatio = currentTime / this.getDuration();
              var $seekBar = $('.seek-control .seek-bar');
              updateSeekPercentage($seekBar, seekBarFillRatio);
-             //setCurrentTimeInPlayerBar(filterTimeCode(currentTime));
+             setCurrentTimeInPlayerBar(currentTime);
          });
      }
  };
@@ -279,12 +283,11 @@ var trackIndex = function(album, song) {
 };
 
 var updatePlayerBarSong = function() {
-
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
-    //setTotalTimeInPlayerBar(filterTimeCode(totalTime));
+    setTotalTimeInPlayerBar(currentSongFromAlbum.duration);
 };
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
